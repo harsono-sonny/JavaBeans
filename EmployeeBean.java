375e8621af9faea1523104a6d195d36fda50aa89
harsono-sonny <sonix911@gmail.com>
2017-05-20 15:30:17 -0600

    Update EmployeeBean.java

--- EmployeeBean.java
+++ EmployeeBean.java
@@ -3,100 +3,16 @@ package model;
 /**
  * Created by sonnyharsono on 5/20/17.
  */
-import java.io.Serializable;
+public class EmployeeBean extends PersonBean {
 
-public class EmployeeBean implements Serializable {
+    private String id;
 
-    private String firstName; //employee first name
-    private String lastName; //employee last name
-    private int startYear; //the year of starting employment
-    private double payRate; //the hourly pay rate
+    public EmployeeBean(String firstName, String lastName, int age, String phoneNumber, String id) {
 
-    // no parameter constructor
-    public EmployeeBean() {
-    }
-
-    /**
-     *
-     * @param firstName
-     * @param lastName
-     * @param startYear
-     * @param payRate
-     */
-
-    // overloaded constructor
-    public EmployeeBean(String firstName, String lastName, int startYear, double payRate) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.startYear = startYear;
-        this.payRate = payRate;
-    }
-
-    /*
-    * private fields with standard getters and setters
-    */
-
-    /**
-     *
-     * @return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    /**
-     *
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    /**
-     *
-     * @return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
-
-    /**
-     *
-     * @param lastName the lastName to set
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /**
-     *
-     * @return the startYear
-     */
-    public int getStartYear() {
-        return startYear;
-    }
-
-    /**
-     *
-     * @param startYear the startYear to set
-     */
-    public void setStartYear(int startYear) {
-        this.startYear = startYear;
-    }
-
-    /**
-     *
-     * @return the payRate
-     */
-    public double getPayRate() {
-        return payRate;
-    }
-
-    /**
-     *
-     * @param payRate the payRate to set
-     */
-    public void setPayRate(double payRate) {
-        this.payRate = payRate;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.id = id;
     }
 }
