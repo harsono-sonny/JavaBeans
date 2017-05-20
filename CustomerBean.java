9e4f2913b980a071135b032d182f00450d810bff
harsono-sonny <sonix911@gmail.com>
2017-05-20 15:14:13 -0600

    Create CustomerBean.java

---
+++ CustomerBean.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Created by sonnyharsono on 5/20/17.
+ */
+public class CustomerBean extends PersonBean {
+
+    public CustomerBean(String firstName, String lastName, int age, String phoneNumber) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+    }
+}
